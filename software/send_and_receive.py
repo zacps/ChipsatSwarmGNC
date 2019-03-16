6b257bb7ba0c5040024ae9f271b03d868d89471b
Ellen <18584146+genericity@users.noreply.github.com>
2019-03-16 16:04:21 +1300

    Separated the to_bits() and from_bits() functions into a separate utils file.

--- software/send_and_receive.py
+++ software/send_and_receive.py
@@ -1,5 +1,6 @@
 from cpc.cpc import *
 import time
+import util
 
 # Constant syncword.
 SYNC_WORD = "666A"
@@ -17,38 +18,17 @@ rx.setupTX()
 rx.setupCheck()
 
 sendBuffer = ["test 123"]
-
-def to_bits(s):
-	result = []
-	print("Converting to bits the string: ")
-	print(s)
-	for c in s:
-		bits = bin(ord(c))[2:]
-		bits = '00000000'[len(bits):] + bits
-		result.extend([str(b) for b in bits])
-	result = ''.join(result)
-	print("Converted bitstring: ")
-	print(result)
-	return result
-
-def from_bits(bits):
-	bits = list(map(bits, int))
-	chars = []
-	for b in range(len(bits) / 8):
-		byte = bits[b*8:(b+1)*8]
-		chars.append(chr(int(''.join([str(bit) for bit in byte]), 2)))
-	return ''.join(chars)
 	
 def receive():
 	# Receive data.
 	data = rx.receiveData(1024)
 	print(data)
-	print(from_bits(data))
+	print(util.from_bits(data))
 
 def send():
 	# Send data if there's something queued.
 	if len(sendBuffer) != 0:
-		rx.sendData(to_bits(sendBuffer.pop(0)), SYNC_WORD)
+		rx.sendData(util.to_bits(sendBuffer.pop(0)), SYNC_WORD)
 
 while True:
 	send()
---
+++ software/util.py
@@ -0,0 +1,22 @@
+# Converts a string s to bits, ready to be passed to cpc.sendData().
+def to_bits(s):
+	result = []
+	print("Converting to bits the string: ")
+	print(s)
+	for c in s:
+		bits = bin(ord(c))[2:]
+		bits = '00000000'[len(bits):] + bits
+		result.extend([str(b) for b in bits])
+	result = ''.join(result)
+	print("Converted bitstring: ")
+	print(result)
+	return result
+
+# Converts a bitstring to a human-readable string.
+def from_bits(bits):
+	bits = list(map(bits, int))
+	chars = []
+	for b in range(len(bits) / 8):
+		byte = bits[b*8:(b+1)*8]
+		chars.append(chr(int(''.join([str(bit) for bit in byte]), 2)))
+	return ''.join(chars)
