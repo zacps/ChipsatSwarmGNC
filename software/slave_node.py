32962f351a9bc4b718b9902a71e081b511ab5c76
clemson <blastprocessor@protonmail.com>
2019-03-17 13:40:43 +1300

    add lumunosity sensing and sending lum data back to master

--- software/slave_node.py
+++ software/slave_node.py
@@ -1,5 +1,18 @@
 from routing_node import RoutingNode
+import adafruit_tcs34725
+import busio
+import struct
+import board
 
 class SlaveNode(RoutingNode):
     def __init__(self):
         RoutingNode.__init__(self)
+        i2c = busio.I2C(board.SCL, board.SDA)
+        self.sensor = adafruit_tcs34725.TCS34725(i2c)
+
+    def on_poll(self):
+        lux = self.sensor.lux
+        print(lux)
+        self.message = bytearray(struct.pack("f",lux))
+        self.send_message()
+		
